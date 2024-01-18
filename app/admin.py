54a5dcf271b036ef79f05afe8e2f4b13eb74717e
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-01-18 10:47:52 +0100

    second commit

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Answer, AnswerNumericData, NumericData, Question, Tag
+
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(NumericData)
+admin.site.register(AnswerNumericData)
--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "app"
 
 urlpatterns = [
-    path('/', views.index, name="dashboard"),
+    path('home', views.index, name="dashboard"),
 
 ]
