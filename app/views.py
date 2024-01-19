66f8c3dd71e93611cd4ab9b834d5aace98eec805
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-01-19 15:15:47 +0100

    Troisième commit

--- app/views.py
+++ app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import Question, Answer, Tag
 # Create your views here.
 
 def index(request):
 
+    questions = Question.objects.all().order_by('-updated_at')
+
+    paginator = Paginator(questions, 25)  # Show 25 contacts per page.
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        "name": "Aure",
+        "questions" : page_obj,
     }
 
     return render(request, "index.html", context)
